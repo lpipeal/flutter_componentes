d08fb0d5ab5c68c84172b42eb74eef4c804176b0
luis felipe alvarez <lpipeal@gmail.com>
2020-01-25 23:39:10 -0500

    Creando listas apartir de un arreglo

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
  
 class HomePage extends StatelessWidget {
+
+  final opciones = ['Uno','Dos','Tres','Cuatro','Cinco','seis','siete','ocho','nueve'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -10,16 +13,35 @@ class HomePage extends StatelessWidget {
           title: Text('Material App Bar'),
         ),
         body: ListView(
-          children: <Widget>[
+          children: _crearItems(),
+                  )
+                ),
+              );
           
+                     
           
+            }
+          
+            List<Widget> _crearItems() {
 
-        ],
-        )
-      ),
-    );
+              List<Widget> lista = new List<Widget>();
 
-    
+                for (String opt in opciones) {
+                  
+                  final tempWidget =ListTile(
+                    
+                    leading: Icon(Icons.account_box),
+                    title: Text(opt),
+                    onTap: (){},
 
-  }
+                    
+                  );
+                  //Metodo en cascada para 
+                  lista..add(tempWidget)
+                       ..add(Divider(height: 50));
+                }
+
+              return lista;
+
+            }
 }
