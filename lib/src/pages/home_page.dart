cdc8f70f7f85a39751b4fe2f0c83348070c767e1
luis felipe alvarez <lpipeal@gmail.com>
2020-01-26 01:29:12 -0500

    Lista asincrona apartir de un json y future builder

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -28,30 +28,55 @@ class HomePage extends StatelessWidget {
 
     Widget _lista() {
 
-      menuProvider.cargarData().then((opciones){
-        print(opciones);
-      });
+      //menuProvider.cargarData().then((opciones){
+        //print(opciones);
+      //});
      
+     return FutureBuilder(
+       future: menuProvider.cargarData(),
+       initialData: [],
+       builder: (BuildContext context, AsyncSnapshot <List<dynamic>>snapshot){
+
+                print('builder');
+                print(snapshot.data);
+
+                 return ListView(
+                    children: _listaItems(snapshot.data),
+                  );       
+
+                 },
+     );
 
 
-        return ListView(
-            children: _listaItems(),
-                    );
+ 
             
                 }
 
 
-                
-            
-              List <Widget>_listaItems() {
-                    return [
-                      ListTile(title: Text('Hola mundo')),
-                      ListTile(title: Text('Hola mundo')),
-                      ListTile(title: Text('Hola mundo')),
-                      ListTile(title: Text('Hola mundo')),
-                      ListTile(title: Text('Hola mundo')),
-                    ];
 
+            
+              List <Widget>_listaItems(List<dynamic> data) {
+                    
+                  final List <Widget> opciones =[];
+
+
+                          data.forEach((opt){ 
+
+                                    final widgetTemp = ListTile(
+                                      title: Text(opt['texto']),
+                                      leading: Icon(Icons.account_circle, color: Colors.blue,),
+                                      trailing: Icon(Icons.keyboard_arrow_right),
+                                      onTap: (){
+                                        
+                                      },
+                                    );
+                                  opciones..add(widgetTemp)
+                                            ..add(Divider(height: 35,));
+                                }
+                          );
+                    
+                  return opciones;
+                  
               }
 
 
